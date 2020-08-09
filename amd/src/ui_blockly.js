be6ecfa8c375776019b28016fab8e06c586c54b4
Ruben Cancho <canchete@gmail.com>
2020-08-09 10:36:41 +0200

    Better comments syntax

--- amd/src/ui_blockly.js
+++ amd/src/ui_blockly.js
@@ -1,12 +1,12 @@
 define(['jquery', 'require', 'core/url', 'qtype_coderunner/blockly/browser'], function($, require, url, Blockly) {
-/* 1. A constructor SomeUiName(textareaId, width, height, params) that
- *    builds an HTML component of the given width and height. textareaId is the
- *    ID of the textArea from which the UI element should obtain its initial
- *    serialisation and to which it should write the serialisation when its save
- *    or destroy methods are called. params is a JavaScript object,
- *    decoded from the JSON templateParams defined by the question plus any
- *    additional data required, such as the 'lang' in the case of Ace.
- */
+
+        // A constructor SomeUiName(textareaId, width, height, params) that
+        // builds an HTML component of the given width and height. textareaId is the
+        // ID of the textArea from which the UI element should obtain its initial
+        // serialisation and to which it should write the serialisation when its save
+        // or destroy methods are called. params is a JavaScript object,
+        // decoded from the JSON templateParams defined by the question plus any
+        // additional data required, such as the 'lang' in the case of Ace.
         function BlocklyUi(textareaId, width, height, templateParams) {
 
             var textArea, locale, toolbox, that;
@@ -62,34 +62,30 @@ define(['jquery', 'require', 'core/url', 'qtype_coderunner/blockly/browser'], fu
             };
         }
 
-/* 2. A getElement() method that returns the HTML element that the
- *    InterfaceWrapper is to insert into the document tree.
- */
+        // A getElement() method that returns the HTML element that the
+        // InterfaceWrapper is to insert into the document tree.
         BlocklyUi.prototype.getElement = function() {
             return this.blocklyDiv;
         };
 
-/* 3. A method failed() that should return true unless the constructor
- *    failed (e.g. because it was not able to de-serialise the text area's
- *    contents). The wrapper will call destroy() on the object if failed()
- *    returns true and abort the use of the UI element. The text area will
- *    have the uiloadfailed class added, which CSS will display in some
- *    error mode (e.g. a red border).
- */
+        // A method failed() that should return true unless the constructor
+        // failed (e.g. because it was not able to de-serialise the text area's
+        // contents). The wrapper will call destroy() on the object if failed()
+        // returns true and abort the use of the UI element. The text area will
+        // have the uiloadfailed class added, which CSS will display in some
+        // error mode (e.g. a red border).
         BlocklyUi.prototype.failed = function() {
             return this.fail;
         };
 
-/* 4. A method failMessage() that will be called only when failed() returns
- *    True. It should be a defined CodeRunner language string key.
- */
+        // A method failMessage() that will be called only when failed() returns
+        // True. It should be a defined CodeRunner language string key.
         BlocklyUi.prototype.failMessage = function() {
 
         };
 
-/* 5. A sync() method that copies the serialised represention of the UI plugin's
- *    data to the related TextArea. This is used when submit is clicked.
- */
+        // A sync() method that copies the serialised represention of the UI plugin's
+        // data to the related TextArea. This is used when submit is clicked.
         BlocklyUi.prototype.sync = function() {
             // Generate and save blocky state
             var workspaceState = Blockly.Xml.workspaceToDom(this.workspace);
@@ -101,10 +97,9 @@ define(['jquery', 'require', 'core/url', 'qtype_coderunner/blockly/browser'], fu
             this.textArea.value = (new XMLSerializer()).serializeToString(workspaceState);
         };
 
-/* 6. A destroy() method that should sync the contents to the text area then
- *    destroy any HTML elements or other created content. This method is called
- *    when CTRL-ALT-M is typed by the user to turn off all UI plugins
- */
+        // A destroy() method that should sync the contents to the text area then
+        // destroy any HTML elements or other created content. This method is called
+        // when CTRL-ALT-M is typed by the user to turn off all UI plugins
         BlocklyUi.prototype.destroy = function() {
             if (this.workspace) {
                 this.workspace.dispose();
@@ -115,9 +110,8 @@ define(['jquery', 'require', 'core/url', 'qtype_coderunner/blockly/browser'], fu
             }
         };
 
-/* 7. A resize(width, height) method that should resize the entire UI element
- *    to the given dimensions.
- */
+        // A resize(width, height) method that should resize the entire UI element
+        // to the given dimensions.
         BlocklyUi.prototype.resize = function(width, height) {
             this.blocklyDiv.style.height = height + "px";
             this.blocklyDiv.style.width = width + "px";
@@ -127,15 +121,13 @@ define(['jquery', 'require', 'core/url', 'qtype_coderunner/blockly/browser'], fu
             }
         };
 
-/* 8. A hasFocus() method that returns true if the UI element has focus.
- */
+        // A hasFocus() method that returns true if the UI element has focus.
         BlocklyUi.prototype.hasFocus = function() {
             // TODO
         };
 
-/* The return value from the module define is a record with a single field
- * 'Constructor' that references the constructor (e.g. Graph, AceWrapper etc)
- */
+       // The return value from the module define is a record with a single field
+       // 'Constructor' that references the constructor (e.g. Graph, AceWrapper etc)
        return {
             Constructor: BlocklyUi
         };
